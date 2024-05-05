05d5707d099cbaa79c5a880006425fe5dce4f73a
OscarWSCW <159733064+OscarWSCW@users.noreply.github.com>
2024-05-06 11:15:16 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-# Asks user their name\
-name = input("What is your name?")
-print(" welcome to quiz", name)
+print("hello")
+# Ask the user their name
+input("What is your name?")
+#Greet the user and welcome them to the quiz
+print("Welcome to this quiz.") 
+print("This quiz is about New Zealand")
+#Ask the user a question
+input("What is the capital of New Zealand?")
+#Tell the user the answer to the question
+print("The answer is Wellington.")
+#End the quiz
+print("Thank you for playing!")
