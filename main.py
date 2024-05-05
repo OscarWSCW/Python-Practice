f20e0c81f02b06e87b4886afd424ccd45ed6d6c0
OscarWSCW <159733064+OscarWSCW@users.noreply.github.com>
2024-05-06 11:56:43 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-# Asks user their name\
-name = input("What is your name?")
-print(" welcome to quiz", name)
+print("hello")
+# Ask the user their name
+input("What is your name?")
+#Greet the user and welcome them to the quiz
+print("Welcome to this quiz.") 
+print("This quiz is about Cities")
+#Ask the user a question
+answer = input("What is the largest city by population?")
+#Tell the user the answer to the question
+if answer == "Tokyo":
+    print ("Correct!")    
+else:
+    print("Wrong!")
+print("The correct answer is Tokyo")
+#End the quiz
+
+print("Thank you for playing!")
